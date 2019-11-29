c457f9e8216d62f2c6d24023441c73b4b3279785
Ponchimeow <trancetoradora@gmail.com>
2019-11-29 16:06:41 +0800

    deploy build

---
+++ build/precache-manifest.3e4453cd778022d55910b7704ff2b1bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6cd3bd713199852f1d6a8775f0c8aa",
+    "url": "/Blog-SPA/index.html"
+  },
+  {
+    "revision": "4990373197856e36b0d8",
+    "url": "/Blog-SPA/static/css/main.e18f37c4.chunk.css"
+  },
+  {
+    "revision": "a293d05652361469d6ba",
+    "url": "/Blog-SPA/static/js/2.f31c531f.chunk.js"
+  },
+  {
+    "revision": "4990373197856e36b0d8",
+    "url": "/Blog-SPA/static/js/main.3beb0d17.chunk.js"
+  },
+  {
+    "revision": "44001eac12ac09011ef0",
+    "url": "/Blog-SPA/static/js/runtime-main.d81be465.js"
+  },
+  {
+    "revision": "80c48668dd36afc1af7f7af1b712baf6",
+    "url": "/Blog-SPA/static/media/blackcat.80c48668.svg"
+  },
+  {
+    "revision": "f831ca65736024082dbfde4336fcec32",
+    "url": "/Blog-SPA/static/media/river.f831ca65.JPG"
+  }
+]);
