9717825b69222d8f2d3434a83f84aae7af7ca36d
Ponchimeow <trancetoradora@gmail.com>
2019-12-12 12:47:42 +0800

    build and deploy

--- build/precache-manifest.5429a5ea7d3472793a9bf38f21409489.js
+++ build/precache-manifest.5429a5ea7d3472793a9bf38f21409489.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "857c236114388e5dc28f0c6ba3b86035",
+    "revision": "97b66d264e9586115358b5a7f9867272",
     "url": "/Blog-SPA/index.html"
   },
   {
-    "revision": "6218771698a86029e987",
+    "revision": "43cc35b0bb602b8e8687",
     "url": "/Blog-SPA/static/css/main.e482ce72.chunk.css"
   },
   {
-    "revision": "a293d05652361469d6ba",
-    "url": "/Blog-SPA/static/js/2.f31c531f.chunk.js"
+    "revision": "d04685f22e84d90111c0",
+    "url": "/Blog-SPA/static/js/2.2e4a4e5f.chunk.js"
   },
   {
-    "revision": "6218771698a86029e987",
-    "url": "/Blog-SPA/static/js/main.3beb0d17.chunk.js"
+    "revision": "43cc35b0bb602b8e8687",
+    "url": "/Blog-SPA/static/js/main.5e9c04ba.chunk.js"
   },
   {
-    "revision": "44001eac12ac09011ef0",
-    "url": "/Blog-SPA/static/js/runtime-main.d81be465.js"
+    "revision": "ca4f574d892504aefb34",
+    "url": "/Blog-SPA/static/js/runtime-main.5ad5aa4c.js"
   },
   {
     "revision": "80c48668dd36afc1af7f7af1b712baf6",
